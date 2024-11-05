526121a87c6ce62a29489c6c02e535df04749b28
pogno02 <pierre.02raffoul@gmail.com>
2024-11-05 11:30:48 +0100

    server.ts, ajout d'exemple d'utilisation des classes

--- fiveMusics/src/server.ts
+++ fiveMusics/src/server.ts
@@ -1,6 +1,11 @@
 import express, {Express, Request, Response} from 'express';
+import { verifyEmailLogin, verifyEmailSingup, verifyPassword, verifyToken } from './middlewares/authentification';
+import { login, signup } from './controleurs/authentification';
+import { addMusic, deleteMusic, getMusic, getMusicsByUserId, getRandomMusic } from './controleurs/musique';
 import cors from 'cors';
+import { getAllOtherUsers, getAllUsers } from './controleurs/utilisateurs';
 import morgan from 'morgan';
+
 
 
 const app : Express = express();
@@ -12,9 +17,25 @@ app.use(cors());  // Ajout du middleware CORS
 
 app.get('/', (req : Request, res : Response) : void => {
     res.status(200).json({
-        "message" : "Bienvenue sur FiveMusics !"
+        "message" : "wsh sur l'application FIVE SONGS !"
     });
 });
+
+// utilisateurs
+app.get('/getUsers', getAllUsers);
+app.get('/getAllOtherUsers', verifyToken, getAllOtherUsers);
+
+// auth
+app.post('/signup', verifyEmailSingup, signup);
+app.post('/login', verifyEmailLogin, verifyPassword, login);
+
+
+// musique
+app.post('/addMusic', verifyToken, addMusic);
+app.get('/getMusic', verifyToken, getMusic);
+app.delete('/deleteMusic/:idMusic', verifyToken, deleteMusic);
+app.get('/getMusicsByUserId/:idUser', verifyToken, getMusicsByUserId);
+app.get('/getRandomMusic', getRandomMusic);
 
 
 
